4ab0290612e8aba4d90f22101cadf10062d8e4ce
Tome Kirov <tome.kirov@symphony-solutions.eu>
2019-07-15 18:55:19 +0200

    rename variables

--- dltwitchclips.py
+++ dltwitchclips.py
@@ -3,19 +3,18 @@ import sys
 import requests
 import urllib.request
 
-var_client_id = 't0e45nrhaq5ayt8z7qoi4xapaeaemi'
-var_basepath = 'tmp/'
-var_full_url = sys.argv[1]
-var_slug = var_full_url.rpartition('/')[-1]
+client_id = 't0e45nrhaq5ayt8z7qoi4xapaeaemi'
+basepath = 'tmp/'
+full_url = sys.argv[1]
+slug = full_url.rpartition('/')[-1]
 
-clip_info = requests.get("https://api.twitch.tv/helix/clips?id=" + var_slug, headers={"Client-ID": var_client_id}).json()
+clip_info = requests.get("https://api.twitch.tv/helix/clips?id=" + slug, headers={"Client-ID": client_id}).json()
 
 thumb_url = clip_info['data'][0]['thumbnail_url']
-var_mp4_url = thumb_url.split("-preview",1)[0] + ".mp4"
-var_out_filename = var_slug + ".mp4"
-output_path = (var_basepath + var_out_filename)
+mp4_url = thumb_url.split("-preview",1)[0] + ".mp4"
+out_filename = slug + ".mp4"
+output_path = (basepath + out_filename)
 
-print(var_mp4_url)
 
 def dl_progress(count, block_size, total_size):
     percent = int(count * block_size * 100 / total_size)
@@ -23,10 +22,10 @@ def dl_progress(count, block_size, total_size):
     sys.stdout.flush()
 
 # create the basepath directory
-if not os.path.exists(var_basepath):
-    os.makedirs(var_basepath)
+if not os.path.exists(basepath):
+    os.makedirs(basepath)
 
 try:
-    urllib.request.urlretrieve(var_mp4_url, output_path, reporthook=dl_progress)
+    urllib.request.urlretrieve(mp4_url, output_path, reporthook=dl_progress)
 except:
     print("An exception occurred")
